48a9a42807adc45eaf7cbd9594047f2e87aff186
Sparkles2000 <rucker.anna2000@gmail.com>
2021-03-30 13:07:51 -0500

    *addedContentTo

---
+++ index.js
@@ -0,0 +1,19 @@
+const dresses = [
+    {Dress: "Sassy Coat Dress", Description: "A classy yet sassy ol skool type of dress that has a modern tone. Sophisticated and stylish at the same time!"},
+    {Dress: "Checkerboard", Description: "Wanna party! A fun party dress that is always in. Party with style."},
+    {Dress: "Simply Divine", Description: "A very Divinely Elegant style dress made for the best of the best. This also is a runway, red carpet style dress so rock this while strutting your stuff!"},
+    {Dress: "Simplicity", Description: "An interpretive dance style dress to inspire how you feel while you dance. This piece is especially inspiring when interpreting sadness. Don't cry though dance instead!"},
+    {Dress: "Fine Wine", Description: "This is another stunning dress that ages like fine wine and is also a runway, red carpet event style dress. Work it honey!"},
+    {Dress: "Velvet Diamond", Description: "Yes, you guessed it! It's another runway, red carpet dress that that is elegant and stunning. very sassy but classy. We're rockin' the diamond status tonight!"}
+]
+const dresses = document.querySelectorAll('.dress')
+    for ( i=0; i < dresses.length; i++) {
+        return(descriptionInput)
+        console.log(descriptionInput)
+    }
+const descriptionInput= document.getElementsByClassName('.dress')
+const descriptionButton= document.getElementsByclassName('.dress')
+function alertDescriptionInputValue() {
+    return(descriptionInput)    
+}
+descriptionButton.addEventListener('click', alertDiscriptionInputValue)
